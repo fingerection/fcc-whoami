f94bc30bb904f95daa15a425ad6035444a85c89f
fingerection <fingerection@gmail.com>
2016-06-09 14:54:48 +0000

    whoam i server

--- server.js
+++ server.js
@@ -1,33 +1,21 @@
-'use strict';
-
 var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
-
 var app = express();
-require('dotenv').load();
-require('./app/config/passport')(passport);
 
-mongoose.connect(process.env.MONGO_URI);
-
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
-
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-routes(app, passport);
+app.get('/api/whoami/', function (req, res) {
+  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  var languageString = req.headers["accept-language"];
+  var uaString = req.headers['user-agent'];
+  
+  var language = languageString.split(';')[0].split(',')[0];
+  var uaMatch = uaString.match(/\([^\)]+\)/)[0];
+  var software = uaMatch.slice(1, uaMatch.length-1)
+  
+  //Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5)
+  res.send({ipaddress: ip, language: language,
+  software: software});
+});
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
+  console.log('Example app listening on port'+port);
 });
